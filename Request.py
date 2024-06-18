65c1eefec63869105629a27a6a2a49506b3648f7
Silver Wolf <131898898+nahida-cn@users.noreply.github.com>
2024-06-19 07:19:58 +0800

    Update Request.py

--- Request.py
+++ Request.py
@@ -1,16 +1,13 @@
 import requests
-import random
+import uuid
 
 while True:
     try:
         UA = {"User-Agent":requests.post("https://www.bejson.com/Bejson/Api/Common/ge_nua?count=1&type=windows").json()["data"][0]}
         Rand = requests.get("https://api.jihujiasuqi.com/apps/captcha/get.php", headers=UA).json()["rand"]
-        for i in range(361):
-            if requests.get("https://api.jihujiasuqi.com/apps/captcha/verify.php?&rand="+Rand+"&angle="+str(i), headers=UA).json()["code"] == 0:
-                Random = ""
-                for v in range(101):
-                    Random = Random+chr(random.randint(97, 122))
-                print(requests.get("https://api.jihujiasuqi.com/api/user.php?mode=reg&mail="+Random+"@163.com&pwd="+Random+"&captcha_rand="+Rand, headers=UA).json()["msg"])
+        for i in range(30, 331, 30):
+            if requests.get(f"https://api.jihujiasuqi.com/apps/captcha/verify.php?&rand={Rand}&angle={i}", headers=UA).json()["code"] == 0:
+                print(requests.get(f"https://api.jihujiasuqi.com/api/user.php?mode=reg&mail={uuid.uuid4()}&captcha_rand="+Rand, headers=UA).json()["msg"])
                 break
     except:
         pass
