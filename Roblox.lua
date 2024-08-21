ee6a88ca17e3669aee36b7afac070d4fe188d0ad
Silver Wolf <131898898+rbxluau@users.noreply.github.com>
2024-08-21 13:50:40 +0800

    Update Roblox.lua

--- Roblox.lua
+++ Roblox.lua
@@ -6,10 +6,10 @@ end
 
 while true do
 	pcall(function()
-		local Rand = GetJson("https://api.jihujiasuqi.com//apps/captcha/get.php").rand
+		local Rand = GetJson("https://api.jihujiasuqi.com/apps/captcha/get.php").rand
 		for i = 30, 330, 30 do
-			if GetJson("https://api.jihujiasuqi.com//apps/captcha/verify.php?rand="..Rand.."&angle="..i).okey then
-				print(GetJson("https://api.jihujiasuqi.com//api/user.php?mode=reg&mail="..HttpService:GenerateGUID(false).."&captcha_rand="..Rand).msg)
+			if GetJson("https://api.jihujiasuqi.com/apps/captcha/verify.php?rand="..Rand.."&angle="..i).okey then
+				print(GetJson("https://api.jihujiasuqi.com/api/user.php?mode=reg&mail="..HttpService:GenerateGUID(false).."&captcha_rand="..Rand).msg)
 				break
 			end
 		end
